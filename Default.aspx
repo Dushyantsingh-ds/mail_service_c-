0764da5b86d6cc80be7fff6c99b16280020e8f0f
Dushyant Singh <dushyantsinghjayash@gmail.com>
2020-08-01 01:39:19 -0700

    C# Project

---
+++ Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    
+<asp:Label ID="Label1" runat="server">From Email Address:   </asp:Label>
+<asp:TextBox ID="txtFrom" runat="server"></asp:TextBox>   
+
+<asp:Label ID="lblPass" runat="server">Email Password:   </asp:Label>
+<asp:TextBox ID="txtPass" runat="server" TextMode="Password" ></asp:TextBox> 
+
+<asp:Label ID="EmailServer" runat="server">Email Server</asp:Label>
+<asp:TextBox ID="txtMailServer" runat="server"></asp:TextBox>    
+
+<asp:Label ID="Label2"  runat="server">Recipient Email Address: </asp:Label>
+<asp:TextBox ID="txtTo" runat="server"></asp:TextBox>
+
+<asp:Label ID="Label3" runat="server">Subject</asp:Label>
+<asp:TextBox ID="txtSubject" runat="server"></asp:TextBox>
+
+<asp:Label ID="Label4" runat="server">Mail Body:</asp:Label>
+<textarea runat="server" id="txtContent"></textarea>
+
+<asp:Button ID="btnSend" runat="server" Text="Send" OnClick="btnSend_Click1"></asp:Button>
+<asp:Label ID="lblStatus" runat="server"> </asp:Label>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net;
+using System.Net.Mail;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    private void btnSend_Click(object sender, System.EventArgs e)
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress(txtFrom.Text); //IMPORTANT: This must be same as your smtp authentication address.
+        mail.To.Add(txtTo.Text);
+
+        //set the content 
+        mail.Subject = txtSubject.Text;
+        mail.Body = txtContent.Value;
+
+
+        //send the message 
+        SmtpClient smtp = new SmtpClient(txtMailServer.Text, 25);
+
+        //IMPORANT:  Your smtp login email MUST be same as your FROM address. 
+        NetworkCredential Credentials = new NetworkCredential(txtFrom.Text, txtPass.Text);
+        smtp.Credentials = Credentials;
+
+        smtp.Send(mail);
+        lblStatus.Text = "Sent email (" + txtSubject.Text + ") to " + txtTo.Text;                            
+    }
+    protected void btnSend_Click1(object sender, EventArgs e)
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress(txtFrom.Text); //IMPORTANT: This must be same as your smtp authentication address.
+        mail.To.Add(txtTo.Text);
+
+        //set the content 
+        mail.Subject = txtSubject.Text;
+        mail.Body = txtContent.Value;
+
+
+        //send the message 
+        SmtpClient smtp = new SmtpClient(txtMailServer.Text, 25);
+
+        //IMPORANT:  Your smtp login email MUST be same as your FROM address. 
+        NetworkCredential Credentials = new NetworkCredential(txtFrom.Text, txtPass.Text);
+        smtp.Credentials = Credentials;
+
+        smtp.Send(mail);
+        lblStatus.Text = "Sent email (" + txtSubject.Text + ") to " + txtTo.Text;
+    }
+}
